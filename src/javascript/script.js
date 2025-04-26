34ebc5440f8bfb7d0c4419b7804625b544a70a7d
xrangel123 <rangelzleonidas@gmail.com>
2025-04-26 00:21:36 -0300

    generate password

---
+++ src/javascript/script.js
@@ -0,0 +1,71 @@
+function getChartTypes() {
+    const uppercase = document.querySelector('#include_uppercase').checked;
+    const lowercase = document.querySelector('#include_lowercase').checked;
+    const number = document.querySelector('#include_number').checked;
+    const specialCharacter = document.querySelector('#include_special_character').checked;
+    const charTypes = [];
+    if(uppercase){
+        charTypes.push('ABCDEFGHIJKLMNOPQRSTUWXYZ')
+    }
+    if(lowercase){
+        charTypes.push('abcdefghijklmnopqrstuwxyz')
+    }
+    if(number){
+        charTypes.push('0123456789')
+    }
+    if(specialCharacter){
+        charTypes.push('@#%&$!+-,*~_:?[]{}')
+    }
+    return charTypes;
+}
+function getPasswordSize() {
+    const size = document.querySelector('#size').value;
+    if(isNaN(size) || size < 4 || size > 128){   
+        message('Tamanho inválido, digite um número entre 4 e 128.','danger');
+    }
+    return size;
+} 
+function randomCharType(charTypes){
+    const randomIndex = Math.floor(Math.random() * charTypes.length);   
+    return charTypes[randomIndex][Math.floor(Math.random() * charTypes[randomIndex].length)];
+}
+function generatePassword(size, charTypes){
+    let passwordGenerated = '';
+    const selectedChars = charTypes.join('');
+    charTypes.forEach(type => {
+        passwordGenerated += type[Math.floor(Math.random() * type.length)];
+    });
+    while (passwordGenerated.length < size){
+        passwordGenerated += selectedChars[Math.floor(Math.random() * selectedChars.length)];
+    }
+    passwordGenerated = passwordGenerated.split('').sort(() => Math.random() - 0.5).join('');
+    return passwordGenerated;
+}
+function message(text, status = 'success') {
+    Toastify({
+        text: text,
+        duration: 2000,
+        style: {
+            background: status === 'success' ? '#84cc16' : '#dc2626',
+            boxShadow: 'none'
+        }
+    }).showToast();
+}
+document.querySelector('#generate').addEventListener('click', function() {
+    const size = getPasswordSize();
+    const charTypes = getChartTypes();
+    if(!size){
+        return;
+    }
+    if(!charTypes.length){
+        message('Selecione pelo menos um tipo de caractere!', 'danger');
+        return;
+    }
+    const passwordGenerated = generatePassword(size, charTypes);
+    document.querySelector('#password_container').classList.add('show');
+    document.querySelector('#password'). textContent = passwordGenerated;
+});
+document.querySelector('#copy').addEventListener('click', function(){
+    navigator.clipboard.writeText(document.querySelector('#password').textContent);
+    message('Senga copiada com sucesso', 'success');
+});
